6b62342784f7c2c0b8448ffdcde972a3d4279e20
Silky <silky@localhost.com>
2026-06-18 11:58:57 +0200

    Add FastAPI application entry point

---
+++ app/main.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+
+app= FastAPI()
+
+@app.get("/")
+def home():
+    return {
+        "message":"Welcome to Employee Insights Platform"
+        }
+
+@app.get("/about")
+def about():
+    return {
+           "project": "Employee Insights Platform",
+            "version": "1.0"
+    }        
