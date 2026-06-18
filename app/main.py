bd6c84f9cbbaf34abe5d1d9846a5a4d143f664f0
Silky <silky@localhost.com>
2026-06-18 12:34:58 +0200

    Add employees list endpoint

--- app/main.py
+++ app/main.py
@@ -1,6 +1,27 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 
 app = FastAPI()
+
+employees = [
+    {
+        "id": 1,
+        "name": "Alice",
+        "department": "HR",
+        "salary": 40000
+    },
+    {
+        "id": 2,
+        "name": "Bob",
+        "department": "IT",
+        "salary": 50000
+    },
+    {
+        "id": 3,
+        "name": "Charlie",
+        "department": "Finance",
+        "salary": 60000
+    }
+]
 
 @app.get("/")
 def home():
@@ -20,4 +41,11 @@ def health():
     return {
         "status": "OK",
         "application": "Employee Insights Platform"
-    }      
+    } 
+
+@app.get("/employees")
+def get_employees():
+    return employees  
+
+ 
+
