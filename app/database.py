7c9f3c96836b80a90ac041f27f8b8a205d80ad7e
Silky <silky@localhost.com>
2026-06-22 11:32:58 +0200

    Configure secure database settings using environment variables

---
+++ app/database.py
@@ -0,0 +1,10 @@
+from dotenv import load_dotenv
+import os
+from sqlalchemy import create_engine
+
+load_dotenv()
+
+DATABASE_URL=os.getenv("DATABASE_URL")
+engine=create_engine(
+    DATABASE_URL,echo=True
+)
--- app/models.py
+++ app/models.py
@@ -1,6 +1,19 @@
-from pydantic import BaseModel
-class Employee(BaseModel) :
-    id:int
-    name:str
-    department:str
-    salary:int
+##from pydantic import BaseModel
+##class Employee(BaseModel) :
+  ##  id:int
+  ##  name:str
+   ## department:str
+   ## salary:int
+
+from sqlalchemy import Integer, String, Column
+from database import engine
+from sqlalchemy.orm import declarative_base
+
+Base = declarative_base()
+
+class Employee(Base):
+    __tablename__="employees"
+    id= Column(Integer,primary_key=True,index=True)
+    name=Column(String)
+    department=Column(String)
+    salary=Column(Integer)
