1ec69c00b15858d29c862a59da6635a5fd94e299
Silky <silky@localhost.com>
2026-06-24 13:02:08 +0200

    Add response models and improve API response structure

--- app/routers/employees.py
+++ app/routers/employees.py
@@ -1,19 +1,19 @@
 from fastapi import APIRouter,HTTPException,Depends
 from sqlalchemy.orm import Session
 from app.database import get_db
-from app.models import Employee ##SQLAlchemy model (Database)
-from app.schemas import EmployeeCreate ## Pydantic model (Request validation)
+from app.schemas import EmployeeCreate,EmployeeResponse ## Pydantic model (Request validation)
 from app.crud import employee as crud
+
 
 
 router= APIRouter()
 
-@router.get("/employees")
+@router.get("/employees",response_model=list[EmployeeResponse])
 def get_employees(db:Session=Depends(get_db)):
     return crud.get_employees(db)
 
  
-@router.get("/employees/{employee_id}")
+@router.get("/employees/{employee_id}",response_model=EmployeeResponse)
 def get_employee(employee_id: int, db:Session=Depends(get_db)):
     employee=crud.get_employee_by_id(db,employee_id)
     if not employee:
@@ -25,6 +25,6 @@ def get_employee(employee_id: int, db:Session=Depends(get_db)):
 def get_departments(db:Session=Depends(get_db)):
     return crud.get_departments(db)  
 
-@router.post("/employees")
+@router.post("/employees",response_model=EmployeeResponse,status_code=201)
 def add_employee(employee: EmployeeCreate, db:Session=Depends(get_db)):
     return crud.create_employee(db,employee)
--- app/schemas.py
+++ app/schemas.py
@@ -5,3 +5,12 @@ class EmployeeCreate(BaseModel):
     name: str
     department: str
     salary: int
+
+class EmployeeResponse(BaseModel):
+    id: int
+    name: str
+    department: str
+    salary: int
+
+class Config:
+    from_attributes = True    
